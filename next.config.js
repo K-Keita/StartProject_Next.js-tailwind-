6473e43df61f02ddd0f89d37ed7aae88a867ffcd
kitagawakeita <k.syarin610@gmail.com>
2022-02-27 23:11:50 +0900

    next/imagesのurl追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "s.yimg.com",
+      "www.reuters.com",
+      "media.wired.com",
+      "www.androidcentral.com",
+      "i.guim.co.uk",
+      "images.macrumors.com",
+    ],
+  },
+};
 
 module.exports = nextConfig
